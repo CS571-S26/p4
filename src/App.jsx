24273e6cfc00281157ac8370470def710ae9786c
MousyYaya <Mousy9231@gmail.com>
2026-04-07 10:40:50 -0500

    added gameDetails page(s)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage.jsx'
+import GameDetail from './pages/GameDetails.jsx';
 import AboutPage from './pages/AboutPage.jsx'
 import NotFoundPage from './pages/NotFoundPage.jsx'
 import Layout from './components/Layout.jsx'
@@ -11,6 +12,7 @@ function App() {
     <Routes>
       <Route element={<Layout />}>
         <Route index element={<HomePage />} />
+        <Route path="/game/:id" element={<GameDetail />} />
         <Route path="about" element={<AboutPage />} />
         <Route path="add" element={<AddGamePage />} />
         <Route path="*" element={<NotFoundPage />} />
