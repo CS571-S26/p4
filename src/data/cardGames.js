29b0376214ebd6dee2d3022ca4eaa25e6b21dda1
MousyYaya <Mousy9231@gmail.com>
2026-04-20 13:30:09 -0500

    idk

--- src/data/cardGames.js
+++ src/data/cardGames.js
@@ -1,9 +1,9 @@
 const cardGames = [
-  { id: 1, name: "President", img: "obama.jpg", num_players: "3+", playtime: "5-10 mins", difficulty: "easy" },
-  { id: 2, name: "First Lady", img: "michelle_obama.jpg", num_players: "4", playtime: "30 minutes", difficulty: "hard" },
-  { id: 3, name: "Vice President", img: "joe_biden.jpg", num_players: "2", playtime: "10 minutes", difficulty: "hard" },
-  { id: 4, name: "Secretary", img: "hillary_clinton.jpg", num_players: "2-3", playtime: "15 minutes", difficulty: "moderate" },
-  { id: 5, name: "Secrdsfdsfetary", img: "hillary_clinton.jpg", num_players: "2-3", playtime: "15 minutes", difficulty: "moderate" }
+  { id: 1, name: "President", img: "obama.jpg", num_players: "3+", playtime: "5-10 mins", difficulty: "easy", favorite: false },
+  { id: 2, name: "First Lady", img: "michelle_obama.jpg", num_players: "4", playtime: "30 minutes", difficulty: "hard", favorite: false  },
+  { id: 3, name: "Vice President", img: "joe_biden.jpg", num_players: "2", playtime: "10 minutes", difficulty: "hard", favorite: false  },
+  { id: 4, name: "Secretary", img: "hillary_clinton.jpg", num_players: "2-3", playtime: "15 minutes", difficulty: "moderate", favorite: false  },
+  { id: 5, name: "Secrdsfdsfetary", img: "hillary_clinton.jpg", num_players: "2-3", playtime: "15 minutes", difficulty: "moderate", favorite: false  }
 ];
 
 export default cardGames;
