ae99699ca3cbb3941881064abad4e8b22d9299d6
Lindy <lindyjchen@gmail.com>
2026-04-27 13:30:06 -0500

    add two playing cards and placeholder descriptions. build

---
+++ src/data/funCards.js
@@ -0,0 +1,19 @@
+const funCards = [
+    {
+        id: 1,
+        name: "Colorado Plateau",
+        img: "colorado_plataeau.png",
+        description: "Lindy's tried and true.",
+        link: "https://www.grandcanyon.org/products/impact-southwest-ply-cards-retro-ranger-grd-cir-1001235"
+    },
+    {
+        id: 2,
+        name: "Chicago Architecture",
+        img: "chicago_arch.png",
+        description: "Arya's new favorite!",
+        link: "https://www.transittees.com/chicago-architecture-playing-cards/"
+    }
+
+];
+
+export default funCards;
--- src/pages/FunCardsPage.jsx
+++ src/pages/FunCardsPage.jsx
@@ -1,20 +1,24 @@
-import cardGames from "../data/cardGames";
-import CardCard from "./CardCard";
+import funCards from "../data/funCards";
+import FunCard from "../components/FunCard";
 import { Container, Row, Col } from "react-bootstrap";
-import { useState, useEffect } from "react";
 
 export default function FunCardsPage() {
-  
-  return (
-    <div className="w-100 h-100 d-flex flex-column align-items-center">
-      <div className="text-center my-4">
-        <h1>Fun Cards</h1>
-      </div>
-      <Container>
-        <Row id="cardGames">
-          <p>Here are some of our favorite card decks and accessories to consider for your next fun card purchase!</p>
-        </Row>
-      </Container>
-    </div>
-  );
+
+    return (
+        <div className="w-100 h-100 d-flex flex-column align-items-center">
+            <div className="text-center my-4">
+                <h1 className="p-3">Fun Cards</h1>
+                <h6>Here are some of our favorite playing cards and accessories to consider for your next fun card purchase!</h6>
+            </div>
+            <Container>
+                <Row id="cardGames" className="justify-content-center">
+                    {funCards.map(card => (
+                        <Col key={card.id} xs="auto">
+                            <FunCard {...card} />
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
+        </div>
+    );
 }
